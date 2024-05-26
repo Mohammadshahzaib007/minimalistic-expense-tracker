c7544dea5df95951dcba369d2898249d9e2041e4
Mohammad_shahzaib07 <shahzaib@quolam.com>
2024-05-26 18:09:36 +0530

    Initial setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text, Center, Heading, Divider } from "@gluestack-ui/themed";
+
+export default function Home() {
+  return (
+    <Center flex={1}>
+      <Heading bold size="2xl">
+        Expo V3
+      </Heading>
+      <Divider marginVertical={30} width="80%" />
+      <Text p="$4">Example below to use gluestack-ui components.</Text>
+    </Center>
+  );
+}
